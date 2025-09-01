53c2fb2b2773d10110f59bd0579592734687f1d8
Noman Saeed <rfns19@gmail.com>
2025-09-01 14:30:49 +0500

    update Chapter_3_String.js

---
+++ Chapter_3_String.js
@@ -0,0 +1,5 @@
+// string is a set of different characters like alphabetes, numeri values , symbols etc. string data act like text content. You write string data in sinngle or double qoutes you also write it in back ticks.
+let x;
+x= new String(`Hello world`);
+//above method can make string data into object data
+console.log(typeof x);
