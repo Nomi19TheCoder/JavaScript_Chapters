4c7ba541fa6da459ea562fc68038c86c20cfb6b9
Noman Saeed <rfns19@gmail.com>
2025-09-01 14:31:28 +0500

    update Chapter_4_Number.js

---
+++ Chapter_4_Number.js
@@ -0,0 +1,25 @@
+let x ;
+x = new Number(2);
+console.log(x);
+
+x= Math.sqrt(x);
+console.log(x);
+
+// above method will give square root the given value of x
+
+x= 1.5567;
+x = Math.round(x);
+console.log(x);
+
+//above method will round the x value according to the fundamental math round off rule
+
+x = 1.23
+x = Math.ceil(x);
+console.log(x);
+
+//above method will round the x value to the closest greater value 
+
+x=1.999;
+x = Math.floor(x);
+console.log(x);
+// above method will round the closest lower value
