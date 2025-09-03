bd7d98c918c84a28c49593045e606ab36573d390
Noman Saeed <rfns19@gmail.com>
2025-09-03 14:42:49 +0500

    add all

--- Chapter_5_Date_obj.js
+++ Chapter_5_Date_obj.js
@@ -1,14 +1,1 @@
-let x;
-let time = new Date();
-time = new Date(10,2,2);
-time = new Date(`2-11-2022`)
-time = new Date();
-x = time.getDate();
-x = time.getHours();
-x = time.getTime();
-x = time.getFullYear();
-x = time.getMilliseconds();
-x = time.getMonth();
-x = time.getUTCHours();
-console.log(x);
-
+git 
